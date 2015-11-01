04b8060ef0f83b38b6082b32eb07e9a7988978c7
Otavio Madalosso <otaviomadalosso@gmail.com>
2015-11-01 02:12:11 -0200

    CSS properties update new js file

---
+++ Django Proj/webfriends/static_in_pro/our_static/js/contact.js
@@ -0,0 +1,10 @@
+$("#id_mensagem").each(function () {
+    var style = $(this).attr('style'),
+        textbox = $(document.createElement('textarea')).attr('style', style);
+        textbox.attr('rows',"7");
+        // textbox.attr('cols',"");
+        textbox.attr('autofocus',false);
+        textbox.attr('resize',false);
+
+    $(this).replaceWith(textbox);
+});
