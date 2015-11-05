8c3fc0996b7626b18b702ba41aa466302c2259a1
Otavio Madalosso <omadalosso@inf.ufsm.br>
2015-11-05 10:08:49 -0200

    task correta

--- Django Proj/webfriends/tasks.py
+++ Django Proj/webfriends/tasks.py
@@ -1,8 +1,33 @@
 from celery import Celery
+import os
+import time
+from models import UsuarioFriends, Execution
 
 experiment = Celery('tasks', backend='amqp',
-                      broker='amqp://guest:guest@localhost:5672//')
+                      broker='amqp://otavio:123@10.1.4.28/host1')
 
 @experiment.task
-def add(x, y):
-     return x + y
+def RunExperiment(query, execution, queryOutputFile):
+    # print("\n Executando a query: %s" % (query))
+    execution.status = 2
+    execution.save()
+    start = time.time()
+    os.system(query)
+    dur = time.time() - start
+    print dur
+    execution.status = 3
+    user = execution.request_by
+    # user.notes.add(nota)
+    user.save()
+    execution.time = dur
+    execution.outputFile = queryOutputFile
+    execution.save()
+
+
+@experiment.task(time_limit=17)
+def teste():
+    start = time.time()
+    print start
+    os.system("sleep 15")
+    dur = time.time() - start
+    print dur
