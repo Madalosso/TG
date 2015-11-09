e18915c54a7473596b6f4d6bd033444058a877dc
root <root@debian-lsc.(none)>
2015-11-09 15:35:39 -0200

    django remote

--- Django Proj/webfriends/tasks.py
+++ Django Proj/webfriends/tasks.py
@@ -1,29 +1,29 @@
 from celery import Celery
 import os
 import time
-#from models import UsuarioFriends, Execution
+from models import UsuarioFriends, Execution
 
 #broker =  user:pw@ip/vhost
 #lsc5
 experiment = Celery('tasks', backend='amqp',
                       broker='amqp://otavio:123@10.1.4.27/host1')
 
-#@experiment.task
-#def RunExperiment(query, execution, queryOutputFile):
+@experiment.task
+def RunExperiment(query, execution, queryOutputFile):
     # print("\n Executando a query: %s" % (query))
-#    execution.status = 2
-#    execution.save()
-#    start = time.time()
-#    os.system(query)
-#    dur = time.time() - start
-#    print dur
-#    execution.status = 3
-#    user = execution.request_by
+    execution.status = 2
+    execution.save()
+    start = time.time()
+    os.system(query)
+    dur = time.time() - start
+    print dur
+    execution.status = 3
+    user = execution.request_by
     # user.notes.add(nota)
-#    user.save()
-#    execution.time = dur
-#    execution.outputFile = queryOutputFile
-#    execution.save()
+    user.save()
+    execution.time = dur
+    execution.outputFile = queryOutputFile
+    execution.save()
 
 
 @experiment.task(time_limit=17)
